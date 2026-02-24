a33ec3bbb77bab48e249912d4d42f46028561c4f
siva.selvanathan <sselvanathan_IE@Dewberry.com>
2026-02-24 12:37:00 -0500

    Updated a code comment for the channel inflow with giuh

--- src/share/modelVarType.f90
+++ src/share/modelVarType.f90
@@ -25,7 +25,7 @@ module modelVarType
     real, dimension(:), allocatable    :: bfs    ! channel baseflow component (mm)
     real, dimension(:), allocatable    :: bfp    ! channel baseflow component (mm)
     real, dimension(:), allocatable    :: bfncc  ! baseflow non-channelcomponent (mm)
-    real, dimension(:), allocatable    :: tci_giuh ! total channel inflow corrected with giuh (mm)
+    real, dimension(:), allocatable    :: tci_giuh ! total channel inflow with giuh (mm)
     real(kind=8), allocatable          :: runoff_queue_mm(:, :) ! aggregated runoff with giuh (mm)
     real, dimension(:), allocatable    :: nwm_ponded_depth ! GIUH based NWM Ponded Depth (mm)
             
