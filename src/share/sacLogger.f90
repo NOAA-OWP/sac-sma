3e904f544b28a02feee635b1891d41f74b430a99
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-06-19 07:34:09 -0700

    Add ewts status level and use ewts constants for level values

--- src/share/sacLogger.f90
+++ src/share/sacLogger.f90
@@ -21,7 +21,15 @@ module sac_log_module
   use logger, only: ewts_write_log_module => write_log_module, &
                     ewts_is_logger_enabled_module => is_logger_enabled_module, &
                     ewts_get_log_level_module => get_log_level_module, &
-                    ewts_logger_init_module => logger_init_module
+                    ewts_logger_init_module => logger_init_module, &
+                    EWTS_NOTSET, &
+                    EWTS_DEBUG, &
+                    EWTS_PERFORM, &
+                    EWTS_INFO, &
+                    EWTS_WARNING, &
+                    EWTS_SEVERE , &
+                    EWTS_FATAL, &
+                    EWTS_STATUS
   use ewts_module_constants, only: EWTS_ID_SAC_SMA
 #else
   use, intrinsic :: iso_c_binding, only: c_long, c_int, c_ptr, c_null_ptr, c_f_pointer, c_associated
@@ -30,6 +38,17 @@ module sac_log_module
   implicit none
   private
 
+
+#ifdef SACSMA_USE_EWTS
+  integer, parameter, public :: NOTSET            = EWTS_NOTSET
+  integer, parameter, public :: LOG_LEVEL_DEBUG   = EWTS_DEBUG
+  integer, parameter, public :: LOG_LEVEL_PERFORM = EWTS_PERFORM
+  integer, parameter, public :: LOG_LEVEL_INFO    = EWTS_INFO
+  integer, parameter, public :: LOG_LEVEL_WARNING = EWTS_WARNING
+  integer, parameter, public :: LOG_LEVEL_SEVERE  = EWTS_SEVERE
+  integer, parameter, public :: LOG_LEVEL_FATAL   = EWTS_FATAL
+  integer, parameter, public :: LOG_LEVEL_STATUS  = EWTS_STATUS
+#else
   integer, parameter, public :: NOTSET            = 0
   integer, parameter, public :: LOG_LEVEL_DEBUG   = 10
   integer, parameter, public :: LOG_LEVEL_PERFORM = 15
@@ -38,9 +57,9 @@ module sac_log_module
   integer, parameter, public :: LOG_LEVEL_SEVERE  = 40
   integer, parameter, public :: LOG_LEVEL_FATAL   = 50
 
-#ifndef SACSMA_USE_EWTS
   logical, parameter :: SACSMA_FALLBACK_LOGGING_ENABLED = .true.
   integer, parameter :: SACSMA_FALLBACK_LOG_LEVEL = LOG_LEVEL_INFO
+
 #endif
 
   public :: write_log
